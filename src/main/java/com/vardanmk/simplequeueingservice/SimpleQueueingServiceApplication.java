10f0bc5de7bcfc0e2839a8defa08db307994732f
Vardan Mkrtchyan <vardan.mkrtchyan@wmg.com>
2022-04-02 19:25:21 +0400

    Initial commit for project setup.

---
+++ src/main/java/com/vardanmk/simplequeueingservice/SimpleQueueingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vardanmk.simplequeueingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleQueueingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleQueueingServiceApplication.class, args);
+	}
+
+}
